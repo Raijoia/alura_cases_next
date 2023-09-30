dd75a51c673eee5a92a3088b7b3063406a68fd6e
Rai Joia <megac154@gmail.com>
2023-09-29 23:38:56 -0300

    initial project with Link and page 404, FAQ

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link"
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>Alura Cases - Pagina de Perguntas FAQ</h1>
+      <Link href="/">
+        Ir para a home
+      </Link>
+    </div>
+  )
+}
