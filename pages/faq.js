9d1154a5663d8428da6fc31aaf0ffd9134609c6b
Rai Joia <megac154@gmail.com>
2023-10-02 16:18:57 -0300

    api for FAQ

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react"
 import Link from "../src/components/Link"
 
-export default function FAQPage() {
+export async function getStaticProps() {
+  // link api
+  const FAQ_API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json"
+
+  // pegando o link API
+  const faq = await fetch(FAQ_API_URL)
+    // transformando em json
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json()
+    })
+    .then((resposta) => {
+      // retornando para a variável faq ficar com o valor
+      return resposta
+    })
+
+  return {
+    props: {
+      // qualquer coisa que passar aqui, será passado como props
+      // passando o resultado como props
+      faq
+    },
+  }
+}
+
+export default function FAQPage({ faq }) {
   return (
     <div>
       <h1>Alura Cases - Pagina de Perguntas FAQ</h1>
-      <Link href="/">
-        Ir para a home
-      </Link>
+      <Link href="/">Ir para a home</Link>
+      <ul>
+        {faq.map(({ answer, question }) => (
+          <li key={question}>
+          <article>
+            <h2>{question}</h2>
+            <p>{answer}</p>
+          </article>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
