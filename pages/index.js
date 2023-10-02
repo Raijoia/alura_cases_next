d2325163d2c2435db08a56b87071adef3ed892f5
Rai Joia <megac154@gmail.com>
2023-10-02 14:36:51 -0300

    styled h1 ok

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
+import React from "react"
 import Link from "../src/components/Link"
+
+function Title({ children, as }) {
+  const Tag = as;
+  return (
+    <React.Fragment>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+          font-family: sans-serif;
+        }
+      `}</style>
+    </React.Fragment>
+  )
+}
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases - Home</h1>
-      <Link href='/faq'>
-        Ir para a Página do FAQ
-      </Link>
+      <Title as="h1">Alura Cases - Home</Title>
+      <Link href="/faq">Ir para a Página do FAQ</Link>
     </div>
   )
 }
